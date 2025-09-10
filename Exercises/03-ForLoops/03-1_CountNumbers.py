6d219499de64ab57396bd5ab07eec4495fdaa938
Philip Forsting <Philip.Forsting@iths.se>
2025-09-10 13:59:25 +0200

    Moved files to Labb1 repo

---
+++ Exercises/03-ForLoops/03-1_CountNumbers.py
@@ -0,0 +1,12 @@
+# Count from -10 to 10 with one increment
+
+sum = 0
+
+# excercise a
+for i in range(-10,11): # 
+    print(f"{i}", end = " ")
+print(f"")
+
+# excercise b
+for i in range(-10,11,2): # 
+    print(f"{i}", end = " ")
